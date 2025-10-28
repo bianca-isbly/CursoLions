73589f2b71465b89bd4b55d20c9574059f1e7b88
bianca-isbly <biancaisabely7@gmail.com>
2025-10-28 11:24:59 -0300

    Adicionando Aula 4 e corrigindo problema de repositório aninhado

---
+++ Aula3/Ex3.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')();
+
+let resposta = prompt("Você gosta de café?");
+
+if (resposta == "sim") {
+    console.log(`Que bom! Café é maravilhoso!`);
+}
+
+if (resposta == "não") {
+    console.log(`Tudo bem! Há outras bebidas interessantes também!`);
+} else {
+    console.log('Apenas responda "sim" ou "não"');
+}
---
+++ Aula4/Ex4.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+let nome = prompt("Como é seu apelido? ");
+let idade = prompt("Qual a sua idade? ");
+let idadeAte18 = 18 - idade;
+let anoNasc = 2025 - idade;
+
+if(idade >= 18){
+    console.log(`${nome}, você é maior de idade, parabéns!`);
+} else{
+    console.log(`${nome}, você é menor de idade... Volte daqui ${idadeAte18} anos.`);
+}
+
+if(idade >= 18){
+    console.log(`Você nasceu no ano de ${anoNasc}, né? :)`);
+}
