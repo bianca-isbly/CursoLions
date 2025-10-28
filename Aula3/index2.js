9196a9d828260e5e651b6f6cc9308298b289387f
bianca-isbly <biancaisabely7@gmail.com>
2025-10-28 09:52:30 -0300

    Corrigido: Removida Aula3 como submódulo e adicionada como pasta normal

---
+++ Aula3/index2.js
@@ -0,0 +1,22 @@
+//Usando if e else
+let numero1 = 20;
+
+if (numero1 == 0) {
+    console.log("O número que você digitou é zero!");
+} else if (numero1 % 2 == 0) {
+    console.log("O número que você digitou é par!");
+} else {
+    console.log("O número que você digitou é ímpar!");
+}
+
+//Usando if e else com prompt
+const prompt = require('prompt-sync')();
+let numero2 = prompt("Digite um número: ");
+
+if (numero2 == 0) {
+    console.log("O número que você digitou é zero!");
+} else if (numero2 % 2 == 0) {
+    console.log("O número que você digitou é par!");
+} else {
+    console.log("O número que você digitou é ímpar!");
+}
