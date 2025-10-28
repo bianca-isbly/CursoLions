a2a5ebcc32c4337c93839ccccbcd1e49c7259070
bianca-isbly <biancaisabely7@gmail.com>
2025-10-28 11:49:23 -0300

    exercício 5

---
+++ Aula4/Ex5.js
@@ -0,0 +1,6 @@
+//let personagensPrincipais = ['Shrek', 'Fiona', 'Príncipe Encantado'];
+//personagensPrincipais.unshift('Burro');
+//console.log(personagensPrincipais);
+
+let minhaMesa = ['Bianca', 'Pedro', 'João', 'Vitória'];
+console.log(`Meu nome é ${minhaMesa[0]}, e meus colegas de mesa são: ${minhaMesa[1]}, ${minhaMesa[2]}, ${minhaMesa[3]}.`);
