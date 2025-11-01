213d78cd13cb3028a8aee1e5f4d7da6eef0de74e
Bianca-isbly <biancaisabely7@gmail.com>
2025-11-01 20:59:02 -0300

    teste

--- Aula5/Ex7.js
+++ Aula5/Ex7.js
@@ -4,19 +4,18 @@ let frete = (valorTotalCompra) + 20;
 console.log(`O valor da sua compra é de R$ ${valorTotalCompra} mais o frete de R$ 20, totalizando o valor de R$ ${frete}`);
 let cupom = prompt("Tem um cupom de desconto? (sim/não)");
 let cupom15 = valorTotalCompra * 0.85;
-let freteZero = valorTotalCompra;
 let cupomsim;
 let nTentivas;
 
 if(cupom == "s" || cupom == "sim" || cupom == "SIM"){
     for (nTentivas = 3; nTentivas > 0; nTentivas = nTentivas - 1) {
         cupomsim = prompt("Insira o cupom de desconto: ");
-        if(cupomsim == "CUPOM15"){
+        if(cupomsim == "CUPOM15" || cupomsim == "cupom15"){
             console.log(`Parabéns! O valor da sua compra foi de R$ ${valorTotalCompra} para o valor de R$ ${cupom15}.`);
             break;
         }
-        else if(cupomsim == "FRETEZERO"){
-            console.log(`Parabéns! O valor da sua compra é de R$ ${freteZero}`);
+        else if(cupomsim == "FRETEZERO" || cupomsim == "fretezero"){
+            console.log(`Parabéns! O valor da sua compra é de R$ ${valorTotalCompra}`);
             break;
         } else {
             console.log(`Cupom inválido. Número de tentivas: ${nTentivas - 1}`);
--- Aula6/Ex9.js
+++ Aula6/Ex9.js
@@ -13,6 +13,6 @@ for (i = 0; i <= 10; i = i + 1) {
 //    console.log(i*tabuada);
 //}
 
-for (i = 0; i <= 10; i = i + 1) {
-    console.log(i+tabuada);
-}
+//for (i = 0; i <= 10; i = i + 1) {
+//    console.log(i+tabuada);
+//}
